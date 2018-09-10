bd417f8ec60ac8b8395a1e461d975b1dc635bd73
Yunho Maeng <yunho.maeng.dc@gmail.com>
2018-09-11 03:57:10 +0900

    ch02 SMU lecture note update

---
+++ 2018_02_SMU_Lecture_Slide/ch02/code/ch02_hello_world.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 25 23:29:09 2016
+
+@author: Yunho
+"""
+
+print "hello world"
+print "hello world "*2
+print "hello" + " World"
+print "hello", "World"
+print "헬로 월드" 
+print "2"*4
