e8290ca05dd17fa15a99ec86aa2acd4bd1ca6c69
Yunho Maeng <yunho.maeng.dc@gmail.com>
2018-09-18 14:47:49 +0900

    add ch03, ch04

---
+++ 2018_02_SMU_Lecture_Slide/ch03/code/format_string_attack.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+"""
+Created on Wed Dec 28 05:31:01 2016
+
+@author: Yunho
+"""
+
+print 'ver 1.1'
+# 개념적인 이해를 위한 코드. 실제로 먹히지 않음
+
+str = raw_input('id:')
+
+# "your id is %d" % 6 과 같은 값을 대입
+# "your id is {0}".format("yunho0130")
+print str
+
+#print "your id is %s" % str
---
+++ 2018_02_SMU_Lecture_Slide/ch04/code/git_develop.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 27 05:19:04 2016
+
+@author: Yunho
+"""
+
+print 'develop'
